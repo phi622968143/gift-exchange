f2fb5c91e4a7eebf3ce6c2be34a064a55bde7e98
KuanYu0729 <george310909@gmail.com>
2024-12-21 23:20:46 +0800

    Add jsdoc & todo

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ const port = 3000;
 	"wishlist": ["book", "toy", "game"]
 }*/
 
+/**
+ * @typedef Gift
+ * @property {number} price 禮物金額
+ */
+
+/**
+ * @desc 參與者
+ * @typedef Participant
+ * @property {number} giftPrice 禮物金額
+ */
+
 const gifts = [
 	{ id: 101, price: 95, name: "book" },
 	{ id: 102, price: 110, name: "game" },
@@ -16,7 +27,13 @@ const gifts = [
 	{ id: 105, price: 160, name: "pen" },
 ];
 
-// Price range +- 20%
+/**
+ * 過濾有效的禮物
+ * Price range +- 20%
+ * @param {Participant} participant 
+ * @param {Gift[]} gifts 
+ * @returns {Gift[]}
+ */
 function filterGifts(participant, gifts) {
 	const minPrice = participant.giftPrice * 0.8;
 	const maxPrice = participant.giftPrice * 1.2;
@@ -27,9 +44,15 @@ function filterGifts(participant, gifts) {
 	);
 }
 
-// Random chose index
+/**
+ * Random choose index
+ * @param {Gift[]} filteredGifts 過濾過的禮物
+ * @returns {Gift | null} 
+ */
 function getRandomGift(filteredGifts) {
-	if (filteredGifts.length === 0) return null;
+	if (filteredGifts.length === 0) {
+		return null;
+	}
 	const randomIndex = Math.floor(Math.random() * filteredGifts.length);
 	return filteredGifts[randomIndex];
 }
@@ -39,10 +62,19 @@ app.post('/api/lucky-draw', (req, res) => {
 	const participant = req.body;
 
 	console.log('Received data:', participant);
-
-	if (!participant || !participant.giftPrice || !Array.isArray(participant.wishlist)) {
+	/**
+	 * @todo 這裡預期 giftPrice 是數字，但缺少了數字檢查
+	 * @todo 如果數字是負數？
+	 */
+	if (
+		!participant ||
+		!participant.giftPrice ||
+		!Array.isArray(participant.wishlist)
+	) {
 		console.error("Missing or invalid data:", participant);
-		return res.status(400).json({ error: "Missing or invalid data." });
+		return res.status(400).json({
+			error: "Missing or invalid data."
+		});
 	}
 
 
